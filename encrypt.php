4f0abfcaebab86a5c9a4fff436b6d147c1175fc2
DanzrV <gamingindo6@gmail.com>
2018-02-17 17:55:37 +0700

    Add feature+ Home
    Feature: 
    Encrypt

---
+++ encrypt.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<link rel="stylesheet" href="encrypt.css">
+    </head>
+<body>
+<form>
+Encrypt :
+<input type="text" name="Encrypt">
+    Decrypt:
+<input type="text" name="Decrypt">
+</form>
+    </body>
+</html>
